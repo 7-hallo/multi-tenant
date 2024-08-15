05b0ded7eb9e60fedeb11070850c22f87f82985b
7zism3 <7zism3@gmail.com>
2024-08-15 20:20:49 +0700

    #setup

--- src/main/java/com/sevenhallo/multitenant/repository/CityRepository.java
+++ src/main/java/com/sevenhallo/multitenant/repository/CityRepository.java
@@ -2,10 +2,11 @@ package com.sevenhallo.multitenant.repository;
 
 import com.sevenhallo.multitenant.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface CityRepository extends JpaRepository<City,Long> {
 
     Optional<City> findById(Long id);
