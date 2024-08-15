3f6d66c012fd3312dee17c560314f0e233d570a6
7zism3 <7zism3@gmail.com>
2024-08-15 20:21:06 +0700

    #setup

---
+++ src/main/java/com/sevenhallo/multitenant/entity/BaseModel.java
@@ -0,0 +1,44 @@
+package com.sevenhallo.multitenant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseModel {
+
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private String lastModifiedBy;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createAt = LocalDateTime.now();
+        this.lastModifiedAt = LocalDateTime.now();
+
+        this.createdBy = "system";
+        this.lastModifiedBy = "system";
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastModifiedAt = LocalDateTime.now();
+
+        this.lastModifiedBy = "system";
+    }
+}
