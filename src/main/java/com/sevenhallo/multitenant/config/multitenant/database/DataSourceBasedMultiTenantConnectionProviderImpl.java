8508e3690ccf0dc33d50f98b514b9932392381fb
7zism3 <7zism3@gmail.com>
2024-08-16 14:37:15 +0700

    #setup

--- src/main/java/com/sevenhallo/multitenant/config/multitenant/database/DataSourceBasedMultiTenantConnectionProviderImpl.java
+++ src/main/java/com/sevenhallo/multitenant/config/multitenant/database/DataSourceBasedMultiTenantConnectionProviderImpl.java
@@ -2,66 +2,45 @@ package com.sevenhallo.multitenant.config.multitenant.database;
 
 import lombok.AllArgsConstructor;
 import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-@Component
+import static com.sevenhallo.multitenant.constant.MultiTenantConstants.DEFAULT_TENANT_ID;
+
+//@Component
 @AllArgsConstructor
-public abstract class DataSourceBasedMultiTenantConnectionProviderImpl extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
+public class DataSourceBasedMultiTenantConnectionProviderImpl extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl<String> {
 
-    private final HashMap<String, DataSource> dataSources = new HashMap<>();
+    private final DataSource defaultDS;
 
-    private final DataSourceConfigRepository configRepo;
+    private final ApplicationContext context;
 
-    public DataSource getDataSource(String name) {
-        if (dataSources.get(name) != null) {
-            return dataSources.get(name);
-        }
-        DataSource dataSource = createDataSource(name);
-        if (dataSource != null) {
-            dataSources.put(name, dataSource);
-        }
-        return dataSource;
+    private Map<String, DataSource> map = new HashMap<>();
+
+    boolean init = false;
+
+    //@PostConstruct
+    public void load() {
+        map.put(DEFAULT_TENANT_ID, defaultDS);
     }
 
-    @PostConstruct
-    public Map<String, DataSource> getAllDataSources() {
-        List<DataSourceConfig> configList = configRepo.findAll();
-        Map<String, DataSource> result = new HashMap<>();
-        for (DataSourceConfig config : configList) {
-            DataSource dataSource = getDataSource(config.getName());
-            result.put(config.getName(), dataSource);
-        }
-        return result;
+    @Override
+    protected DataSource selectAnyDataSource() {
+        return map.get(DEFAULT_TENANT_ID);
     }
 
-    private DataSource createDataSource(String name) {
-        DataSourceConfig config = configRepo.findByName(name);
-        if (config != null) {
-            DataSourceBuilder factory = DataSourceBuilder
-                    .create().driverClassName(config.getDriverClassName())
-                    .username(config.getUsername())
-                    .password(config.getPassword())
-                    .url(config.getUrl());
-            DataSource ds = factory.build();
-            if (config.isInitialize()) {
-                initialize(ds);
-            }
-            return ds;
+    @Override
+    protected DataSource selectDataSource(String tenantIdentifier) {
+        if (!init) {
+            init = true;
+            TenantDataSource tenantDataSource = context.getBean(TenantDataSource.class);
+            map.putAll(tenantDataSource.getAllDataSources());
         }
-        return null;
-    }
-
-    private void initialize(DataSource dataSource) {
-        // ClassPathResource schemaResource = new ClassPathResource("schema.sql");
-        // ClassPathResource dataResource = new ClassPathResource("data.sql");
-        // ResourceDatabasePopulator populator = new ResourceDatabasePopulator(schemaResource, dataResource);
-        // populator.execute(dataSource);
+        return map.get(tenantIdentifier) != null ? map.get(tenantIdentifier) : map.get(DEFAULT_TENANT_ID);
     }
 }
