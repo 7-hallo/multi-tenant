1126a1d07e5140035e6bf0e29f6db6d6ddcb2f7e
7zism3 <7zism3@gmail.com>
2024-08-26 13:50:28 +0700

    #setup

--- src/main/java/com/sevenhallo/multitenant/MultiTenantApplication.java
+++ src/main/java/com/sevenhallo/multitenant/MultiTenantApplication.java
@@ -2,8 +2,15 @@ package com.sevenhallo.multitenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+@ComponentScan(basePackages = {"com.sevenhallo.multitenant", "com.sevenhallo.example"})
+@EnableJpaRepositories({"com.sevenhallo.multitenant", "com.sevenhallo.example"})
+@EntityScan(basePackages = {"com.sevenhallo.multitenant", "com.sevenhallo.example"})
 public class MultiTenantApplication {
 
     public static void main(String[] args) {
